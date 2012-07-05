c5f84ee3ce4454118d75fd10ca17e258c7e219df
Andrey Kutejko <aku@anahoret.com>
2012-07-05 11:03:27 +0300

    tune for osx and nox emacs 22

--- emacs.el
+++ emacs.el
@@ -7,7 +7,7 @@
                   (interactive)
                   (kill-buffer (current-buffer))))
 
-(global-set-key [C-f9] 
+(global-set-key [C-f9]
                 (lambda ()
                   (interactive)
                   (if defining-kbd-macro
@@ -15,11 +15,20 @@
                     (start-kbd-macro nil))))
 (global-set-key [f9] 'call-last-kbd-macro)
 
+;; osx key bindings
+(when (eq system-type 'darwin)
+  (global-set-key '[(kp-delete)] 'delete-char)
+  (global-set-key (kbd "<home>") 'beginning-of-line)
+  (global-set-key (kbd "<end>") 'end-of-line)
+  (setq mac-command-modifier 'meta))
+
 ;; file-name-directory
 (setq dot-emacs-basedir (concat (file-name-directory (file-truename "~/.emacs.el")) "third-party/"))
 
 (add-to-list 'load-path dot-emacs-basedir)
 
+(unless (fboundp 'global-linum-mode)
+  (require 'linum))
 (global-linum-mode 1)
 
 ;; подсветка всех окружающих скобок
@@ -33,11 +42,13 @@
 ;; remove splash
 (setq inhibit-startup-message t)
 ;; remove toolbar
-(tool-bar-mode -1)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
 ;; remove menu
 (menu-bar-mode -1)
 ;; remove scrolls
-(scroll-bar-mode -1)
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
 
 ;; Использовать окружение UTF-8
 (set-language-environment 'UTF-8)
@@ -59,61 +70,72 @@
 (column-number-mode 1)
 
 ;; color theme
+(add-to-list 'load-path (concat dot-emacs-basedir "color-theme-6.6.0/"))
 (require 'color-theme)
 (color-theme-initialize)
 (color-theme-charcoal-black)
 
 ;; expand frame
 (when window-system
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+  (cond ((string= system-type "darwin")
+         (set-frame-position (selected-frame) 100 0)
+         (set-frame-size (selected-frame) 200 1000))
 
+        (t ; unix
+         (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+         (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))))
+
+;;;
 ;;; slime
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-;; Replace "sbcl" with the path to your implementation
-(setq inferior-lisp-program "sbcl")
+;;;
+(let ((ql (expand-file-name "~/quicklisp/slime-helper.el")))
+  (when (file-exists-p ql)
+    (load ql)
+    ;; Replace "sbcl" with the path to your implementation
+    (setq inferior-lisp-program "sbcl")))
 
 ;;
 ;; tabbar
 ;;
-(when (require 'tabbar nil t)
+(when (and window-system
+           (require 'tabbar nil t))
   (defun is-emacs-buffer (name)
     (or
      (string= (substring name 0 2) " *")
      (string= (substring name 0 1) "*")))
 
   (setq tabbar-buffer-groups-function
-	(lambda (buffer)
-	  (with-current-buffer (get-buffer buffer)
-	    (cond
-	     ((is-emacs-buffer (buffer-name))
-	      '("Emacs Buffers"))
-	     (t
-	      '("User Buffers"))))))
+        (lambda (buffer)
+          (with-current-buffer (get-buffer buffer)
+            (cond
+             ((is-emacs-buffer (buffer-name))
+              '("Emacs Buffers"))
+             (t
+              '("User Buffers"))))))
   (tabbar-mode)
 
   (global-set-key (kbd "<C-tab>") 'tabbar-forward-tab)
   (global-set-key (kbd "<C-S-iso-lefttab>") 'tabbar-backward-tab)
 
   (set-face-attribute 'tabbar-default-face nil
-		      :height 1.5
-		      :background "#B8B8B8")
+                      :height 1.5
+                      :background "#B8B8B8")
 
   (set-face-attribute 'tabbar-button-face nil
-		      :height 1.5
-		      :box '(:line-width 4 :color "#B8B8B8" :style nil))
-  
+                      :height 1.5
+                      :box '(:line-width 4 :color "#B8B8B8" :style nil))
+
   (set-face-attribute 'tabbar-selected-face nil
-		      :foreground "white" :background "#333333"
-		      :height 1.5
-		      :font "DejaVu Sans" :weight 'semi-bold
-		      :box nil)
-  
+                      :foreground "white" :background "#333333"
+                      :height 1.5
+                      :font "DejaVu Sans" :weight 'semi-bold
+                      :box nil)
+
   (set-face-attribute 'tabbar-unselected-face nil
-		      :foreground "black" :background "#EEEEEE"
-		      :height 1.5
-		      :font "DejaVu Sans"
-		      :box '(:line-width 1 :color "#333333" :style nil)))
+                      :foreground "black" :background "#EEEEEE"
+                      :height 1.5
+                      :font "DejaVu Sans"
+                      :box '(:line-width 1 :color "#333333" :style nil)))
 
 (setq enable-local-variables :all)
 
